b051705e64c43a922357581414cf7198b119496a
Michał Dobaczewski <mdobak@gmail.com>
2023-11-08 14:09:40 +0100

    Remove unnecessary quotes in some log fields [sc-3204] (#287)

--- pkg/util/dump/dump.go
+++ pkg/util/dump/dump.go
@@ -1,11 +1,13 @@
 package dump
 
 import (
+	"bytes"
 	"encoding"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 const dumpMaxDepth = 64
@@ -17,25 +19,20 @@ const dumpMaxDepth = 64
 // human-readable format.
 //
 //   - Simple types, like numbers and strings and booleans, are returned as-is.
-//   - For types that implement json.Marshaler, the result of MarshalJSON is
-//     returned.
+//   - For types that implement fmt.Stringer, the result of String is returned.
 //   - For types that implement encoding.TextMarshaler, the result of
 //     MarshalText is returned.
-//   - For types that implement fmt.Stringer, the result of String is returned.
+//   - For types that implement json.Marshaler, the result of MarshalJSON is
+//     returned. If the result is a JSON string, number, or boolean, it is
+//     converted to a Go type.
 //   - Byte slices and arrays are represented as hex strings.
 //   - For types that implement  error, the result of Error is returned.
-//   - In maps, slices, and arrays, each element is recursively normalized
+//   - In maps, slices, and arrays, each element is recursively converted
 //     according to these rules and then represented as a JSON.
+//
+// If a returned value is a JSON, it is returned as a json.RawMessage.
 func Dump(v any) any {
-	v = dump(v, dumpMaxDepth)
-	if isSimpleType(v) {
-		return v
-	}
-	b, err := json.Marshal(v)
-	if err != nil {
-		return fmt.Sprintf("<error: %v>", err)
-	}
-	return json.RawMessage(b)
+	return dump(v, dumpMaxDepth)
 }
 
 //nolint:gocyclo,funlen
@@ -48,37 +45,31 @@ func dump(v any, depth int) (ret any) {
 	if depth <= 0 {
 		return "<max depth reached>"
 	}
-	if isNil(v) {
-		return nil
-	}
 	if isSimpleType(v) {
 		return v
 	}
+	if isNil(v) {
+		return nil
+	}
 	switch t := v.(type) {
+	case fmt.Stringer:
+		return t.String()
 	case json.RawMessage:
-		return t
-	case json.Marshaler:
-		b, err := t.MarshalJSON()
-		if err != nil {
-			return fmt.Sprintf("<error: %v>", err)
-		}
-		return json.RawMessage(b)
+		return fromJSON(t)
 	case encoding.TextMarshaler:
 		b, err := t.MarshalText()
 		if err != nil {
 			return fmt.Sprintf("<error: %v>", err)
 		}
 		return string(b)
-	case fmt.Stringer:
-		return t.String()
+	case json.Marshaler:
+		return fromJSON(toJSON(t))
 	case error:
 		return t.Error()
-	case []byte:
-		return "0x" + hex.EncodeToString(t)
 	default:
 		rv := reflect.ValueOf(v)
-		if v == nil || rv.IsZero() {
-			return nil
+		if !rv.IsValid() {
+			return "<invalid value>"
 		}
 		rt := rv.Type()
 		switch rv.Kind() {
@@ -89,25 +80,65 @@ func dump(v any, depth int) (ret any) {
 					m[rt.Field(n).Name] = dump(rv.Field(n).Interface(), depth-1)
 				}
 			}
-			return m
+			return toJSON(m)
 		case reflect.Slice, reflect.Array:
+			if rt.Elem().Kind() == reflect.Uint8 {
+				if !rv.CanAddr() {
+					cpy := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(byte(0))), rv.Len(), rv.Len())
+					reflect.Copy(cpy, rv)
+					rv = cpy
+				}
+				return "0x" + hex.EncodeToString(rv.Bytes())
+			}
 			var m []any
 			for i := 0; i < rv.Len(); i++ {
 				m = append(m, dump(rv.Index(i).Interface(), depth-1))
 			}
-			return m
+			return toJSON(m)
 		case reflect.Map:
 			m := map[string]any{}
 			for _, k := range rv.MapKeys() {
 				m[fmt.Sprint(dump(k, depth-1))] = dump(rv.MapIndex(k).Interface(), depth-1)
 			}
-			return m
+			return toJSON(m)
 		case reflect.Ptr, reflect.Interface:
 			return dump(rv.Elem().Interface(), depth-1)
 		default:
 			return fmt.Sprintf("%v", v)
 		}
 	}
+}
+
+func toJSON(v any) any {
+	b, err := json.Marshal(v)
+	if err != nil {
+		return fmt.Sprintf("<error: %v>", err)
+	}
+	return json.RawMessage(b)
+}
+
+func fromJSON(v any) any {
+	if v == nil {
+		return nil
+	}
+	if b, ok := v.(json.RawMessage); ok {
+		if bytes.HasPrefix(b, []byte{'"'}) {
+			if s, err := strconv.Unquote(string(b)); err == nil {
+				return s
+			}
+			return v
+		}
+		if i, err := strconv.ParseInt(string(b), 10, 64); err == nil {
+			return i
+		}
+		if f, err := strconv.ParseFloat(string(b), 64); err == nil {
+			return f
+		}
+		if b, err := strconv.ParseBool(string(b)); err == nil {
+			return b
+		}
+	}
+	return v
 }
 
 func isSimpleType(v any) bool {
--- pkg/util/dump/dump_test.go
+++ pkg/util/dump/dump_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSerialize(t *testing.T) {
+func TestDump(t *testing.T) {
 	tests := []struct {
 		arg  interface{}
 		want interface{}
@@ -17,17 +17,76 @@ func TestSerialize(t *testing.T) {
 		{arg: nil, want: nil},
 		{arg: 1, want: 1},
 		{arg: 1.1, want: 1.1},
+		{arg: true, want: true},
 		{arg: "foo", want: "foo"},
-		{arg: []byte{0xDE, 0xAD, 0xBE, 0xEF}, want: "0xdeadbeef"},
-		{arg: []string{"foo", "bar"}, want: json.RawMessage(`["foo","bar"]`)},
-		{arg: map[string]string{"foo": "bar"}, want: json.RawMessage(`{"foo":"bar"}`)},
+		{arg: json.RawMessage(`"foo"`), want: "foo"},
+		{arg: stringer{}, want: "foo"},
+		{arg: textMarshaler{v: "foo"}, want: "foo"},
+		{arg: textMarshaler{e: errors.New("foo")}, want: "<error: foo>"},
+		{arg: jsonMarshaler{v: `"foo"`}, want: "foo"},
+		{arg: jsonMarshaler{v: `42`}, want: int64(42)},
+		{arg: jsonMarshaler{v: `3.14`}, want: float64(3.14)},
+		{arg: jsonMarshaler{v: `true`}, want: true},
+		{arg: jsonMarshaler{e: errors.New("foo")}, want: "<error: json: error calling MarshalJSON for type dump.jsonMarshaler: foo>"},
+		{arg: errors.New("foo"), want: "foo"},
 		{arg: struct{ A int }{A: 1}, want: json.RawMessage(`{"A":1}`)},
 		{arg: &struct{ A int }{A: 1}, want: json.RawMessage(`{"A":1}`)},
-		{arg: errors.New("foo"), want: "foo"},
+		{arg: []byte{0xDE, 0xAD, 0xBE, 0xEF}, want: "0xdeadbeef"},
+		{arg: [4]byte{0xDE, 0xAD, 0xBE, 0xEF}, want: "0xdeadbeef"},
+		{arg: [4]string{"foo", "bar", "baz", "qux"}, want: json.RawMessage(`["foo","bar","baz","qux"]`)},
+		{arg: []string{"foo", "bar"}, want: json.RawMessage(`["foo","bar"]`)},
+		{arg: map[string]string{"foo": "bar"}, want: json.RawMessage(`{"foo":"bar"}`)},
+		{arg: emptyInterface(), want: nil},
+		{arg: panicer{}, want: "<panic: foo>"},
 	}
 	for n, tt := range tests {
 		t.Run(fmt.Sprintf("case-%d", n), func(t *testing.T) {
 			assert.Equal(t, tt.want, Dump(tt.arg))
 		})
 	}
+}
+
+func TestMaxDepth(t *testing.T) {
+	r := recursive{}
+	r.A = &r
+	assert.Contains(t, fmt.Sprintf("%s", Dump(r)), "max depth reached")
+}
+
+type stringer struct{}
+
+func (stringer) String() string {
+	return "foo"
+}
+
+type textMarshaler struct {
+	v string
+	e error
+}
+
+func (t textMarshaler) MarshalText() ([]byte, error) {
+	return []byte(t.v), t.e
+}
+
+type jsonMarshaler struct {
+	v string
+	e error
+}
+
+func (j jsonMarshaler) MarshalJSON() ([]byte, error) {
+	return []byte(j.v), j.e
+}
+
+type recursive struct {
+	A *recursive
+}
+
+type panicer struct{}
+
+func (panicer) String() string {
+	panic("foo")
+}
+
+func emptyInterface() fmt.Stringer {
+	var v *stringer
+	return v
 }
