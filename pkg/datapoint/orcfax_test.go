7039007fcc4127c8daf43d128aed06655e2b84b7
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2024-02-28 15:09:13 +0100

    Re-instantiate some Gofer testing
    Tests specific to Orcfax's changes to Gofer are reinstantiated.
    Additional tests will be added (or removed) on a longer timeline.
    
    Tests impacting CI that do not impact Orcfax's required functionality
    have been removed.

--- pkg/datapoint/orcfax_test.go
+++ pkg/datapoint/orcfax_test.go
@@ -10,13 +10,14 @@ import (
 
 func TestReadBuildProperties(t *testing.T) {
 	ldFlags := debug.BuildSetting{
-		"-ldflags", "-s -w -X main.version=100.0.0-SNAPSHOT-057f3fc -X main.commit=057f3fc6318d1824148bf91de5ef674fe8b9a504 -X main.date=2024-01-29T19:14:07Z -X main.builtBy=goreleaser",
+		Key:   "-ldflags",
+		Value: "-s -w -X main.version=100.0.0-SNAPSHOT-057f3fc -X main.commit=057f3fc6318d1824148bf91de5ef674fe8b9a504 -X main.date=2024-01-29T19:14:07Z -X main.builtBy=goreleaser",
 	}
 	res := parseBuildProperties([]debug.BuildSetting{ldFlags})
 	expected := value.BuildProperties{
-		"057f3fc6318d1824148bf91de5ef674fe8b9a504",
-		"100.0.0-SNAPSHOT-057f3fc",
-		"2024-01-29T19:14:07Z",
+		Commit:  "057f3fc6318d1824148bf91de5ef674fe8b9a504",
+		Version: "100.0.0-SNAPSHOT-057f3fc",
+		Date:    "2024-01-29T19:14:07Z",
 	}
 	if res != expected {
 		t.Errorf(
